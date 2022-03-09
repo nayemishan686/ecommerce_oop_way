74e8bc44bb4a49ad051213cfa0d32f2a07adc1f7
Nayem <nayemishan377@gmail.com>
2022-03-09 17:53:08 +0600

    Dashboard packet added and setup for nest step

--- admin/includes/header-nav.php
+++ admin/includes/header-nav.php
@@ -17,7 +17,7 @@
                            </div>
                        </div>
                        <a href="index.html">
-                           <img class="img-fluid" src="assets/images/logo.png" alt="Theme-Logo" />
+                           <img class="img-fluid" src="assets/images/nayem.png" alt="Theme-Logo" />
                        </a>
                        <a class="mobile-options">
                            <i class="ti-more"></i>
